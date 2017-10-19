6f225d977adaad92255b7ef485b345b8abe4882f
Alexander Smirnov <dutch92@mail.ru>
2017-10-19 07:50:15 +0300

    templates for genre

---
+++ js/screens/genre/main.js
@@ -0,0 +1,23 @@
+import createNode from '../../utils/createNode.js';
+import getGenreTmpl from './template.js';
+import {checkState, checkAnswer} from '../../functions/check.js';
+
+export default (state) => {
+  const genreElement = createNode(getGenreTmpl(state));
+
+  genreElement.querySelectorAll(`input[name="answer"]`).forEach((el, i, arr) => {
+    el.onclick = (evt) => {
+      evt.target.checked = !evt.target.checked;
+      genreElement.querySelector(`button.genre-answer-send`).disabled = !arr.some((el) => el.checked);
+    };
+  });
+
+  genreElement.querySelector(`button:not([attr="disabled"]).genre-answer-send`).onclick = (evt) => {
+    evt.preventDefault();
+
+    const answers = genreElement.querySelectorAll(`input[type="checkbox"]:checked`).map((el) => el.value);
+
+    checkAnswer(state, answers);
+    checkState(state);
+  };
+};
