65e525d89f820e5ff39f4f163ee88a84d032ac4c
Alexander <dutch92@mail.ru>
2017-11-03 21:52:46 +0300

    add router in location

--- js/App.js
+++ js/App.js
@@ -2,16 +2,60 @@ import welcome from './modules/welcome';
 import game from './modules/game';
 import result from './modules/result';
 
+const router = {
+  WELCOME: ``,
+  GAME: `game`,
+  RESULT: `result`
+};
+
+const routes = {
+  [router.WELCOME]: welcome,
+  [router.GAME]: game,
+  [router.RESULT]: result
+};
+
+const marshal = (state) => {
+  return JSON.stringify(state);
+};
+
+const unmarshal = (data) => {
+  try {
+    return JSON.parse(data);
+  } catch (err) {
+    return {};
+  }
+};
+
 export default class App {
-  init() {
-    welcome.init();
+  static init() {
+    const hashChangeHandler = () => {
+      const hashValue = location.hash.replace(`#`, ``);
+      const [id, data] = hashValue.split(`?`);
+
+      this.changeRoute(id, data);
+    };
+    window.onhashchange = hashChangeHandler;
+    hashChangeHandler();
   }
 
-  startGame() {
-    game.init();
+  static showWelcome() {
+    location.hash = router.WELCOME;
   }
 
-  showResult(gameResult) {
-    result.init(gameResult);
+  static showGame() {
+    location.hash = router.GAME;
+  }
+
+  static showResult(gameResult) {
+    location.hash = `${router.RESULT}?${marshal(gameResult)}`;
+  }
+
+  static changeRoute(id, data) {
+    const presenter = routes[id];
+    if (presenter) {
+      presenter.init(unmarshal(data));
+    }
   }
 }
+
+App.init();
